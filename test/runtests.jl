1df658346be65108a24b591b92c8a9885e376d41
Spencer Russell <spencer.f.russell@gmail.com>
2016-09-29 01:53:17 -0400

    tweaks tests to pass with new unitless SampledSignals

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using MP3
+using SampledSignals
 
 if VERSION >= v"0.5.0-dev+7720"
     using Base.Test
@@ -13,11 +14,10 @@ DELAY_THRESHOLD = 1160
 # write your own tests here
 @testset "Loading MP3" begin
     reference = load(joinpath(dirname(@__FILE__), "Sour_Tennessee_Red_Sting.mp3"))
-    @test typeof(reference) == SampledSignals.SampleBuf{MP3.PCM16Sample,2,MP3.Hertz}
+    @test typeof(reference) == SampledSignals.SampleBuf{PCM16Sample,2}
     @test size(reference, 2) == 2
     @test abs(size(reference, 1) - 245376) <= DELAY_THRESHOLD
-    @test reference.samplerate == 44100Hz
-    @test reference.samplerate == 44.1kHz
+    @test reference.samplerate == 44100
 end
 
 @testset "Saving MP3" begin
@@ -33,7 +33,7 @@ end
     for samplerate in [8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000]
         save(outpath, reference; samplerate = samplerate)
         audio = load(outpath)
-        @test audio.samplerate == samplerate * Hz
+        @test audio.samplerate == samplerate
     end
 
     for bitrate in [96, 128, 160, 192, 224, 256, 320]
@@ -58,13 +58,13 @@ end
     f32 = map(Float32, reference)
     save(outpath, f32)
     audio = load(outpath)
-    @test audio.samplerate == 44100Hz
+    @test audio.samplerate == 44100
     @test size(audio, 2) == size(reference, 2)
     @test abs(size(audio, 1) - size(reference, 1)) <= DELAY_THRESHOLD
 
     f64 = map(Float32, reference)
     save(outpath, f64)
-    @test audio.samplerate == 44100Hz
+    @test audio.samplerate == 44100
     @test size(audio, 2) == size(reference, 2)
     @test abs(size(audio, 1) - size(reference, 1)) <= DELAY_THRESHOLD
 end
