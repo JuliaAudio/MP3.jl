a68191353fa7bfc35cb463a9c61a4c44cdddfba6
Tony Kelman <tony@kelman.net>
2017-05-22 21:05:20 -0700

    clean up after temporary file created in tests
    don't merge this until it passes on appveyor (it might not)

--- test/runtests.jl
+++ test/runtests.jl
@@ -62,4 +62,6 @@ end
     @test audio.samplerate == 44100
     @test size(audio, 2) == size(reference, 2)
     @test abs(size(audio, 1) - size(reference, 1)) <= DELAY_THRESHOLD
+    
+    rm(outpath)
 end
