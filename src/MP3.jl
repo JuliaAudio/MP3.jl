4c7b9dfabbab228a39687064db8c7fce088f8046
Clouds Flowing <clouds.flowing@gmail.com>
2021-06-11 14:05:28 +0800

    fix for julia 1.6

--- src/MP3.jl
+++ src/MP3.jl
@@ -1,32 +1,29 @@
-__precompile__()
-
 module MP3
-
-deps = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-isfile(deps)? include(deps) : error("MP3 is not properly installed. Please run: Pkg.build(\"MP3\")")
 
 # package code goes here
 using SampledSignals
 using FileIO
 using FixedPointNumbers
+using LinearAlgebra
+using mpg123_jll
+using LAME_jll
 
 # methods to override
 import SampledSignals: nchannels, nframes, samplerate, unsafe_read!, unsafe_write
-import FileIO: load, save
 
 # re-export
 export load, save
 export Hz, kHz, s
 
-type MP3INFO
+struct MP3INFO
     nframes::Int64
     nchannels::Int32
-    samplerate::Int32
+    samplerate::Int64
     datatype::DataType
 end
 
 """create an MP3INFO object from given audio buffer"""
-function MP3INFO{T}(buf::SampleBuf{T})
+function MP3INFO(buf::SampleBuf{T}) where {T}
     MP3INFO(nframes(buf), nchannels(buf), samplerate(buf), T)
 end
 
